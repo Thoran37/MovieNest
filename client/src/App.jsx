ade103128a7b2fdf5e52faa08e23e077eac2cdd5
Thoran37 <thoranmuvvala@gmail.com>
2024-08-05 21:55:13 +0530

    Checking done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button"
-
 export default function App() {
   return (
     <div className="text">      
-      <Button>Click me</Button> 
+      <button>Hello</button>
     </div>
   )
 }
