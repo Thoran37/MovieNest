9b0d453987709342bbdb2bde8954311eff3a08e7
Thoran37 <thoranmuvvala@gmail.com>
2024-08-05 21:28:45 +0530

    Added tailwind and installed shadcn components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 export default function App() {
   return (
-    <div className="text">
-      
+    <div className="text">      
+      <Button>Click me</Button> 
     </div>
   )
 }
