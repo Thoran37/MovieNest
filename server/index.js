337c058fda4d8cd7f0525c1a1e3cf12d954e7e67
Muvvala Thoran Chandra <132071612+Thoran37@users.noreply.github.com>
2024-12-23 22:28:52 +0530

    Added cors origin

--- server/index.js
+++ server/index.js
@@ -20,7 +20,11 @@ app.use(morgan("common"));
 
 // CORS
 const cors = require('cors')
-app.use(cors());
+app.use(cors({
+  origin:{"https://movie-nest-three.vercel.app/"},
+  methods:["POST","GET"],
+  credentials:true
+}));
 
 // Environment variables for secrecy
 require('dotenv').config()
