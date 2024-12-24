6cea5a26350c415ad6ecb7f591e1fbfac4874dc5
Muvvala Thoran Chandra <132071612+Thoran37@users.noreply.github.com>
2024-12-24 16:10:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,11 +37,11 @@ app.use(exp.static(path.join(__dirname, '../client/build')))
 app.use('/assets', exp.static(path.join(__dirname, 'public/assets')));
 
 // Database connection
-const moviedb, users, admins, theatresObj, moviesObj, shows, reservations
+let moviedb, users, admins, theatresObj, moviesObj, shows, reservations
 mongodb.connect(process.env.DB_URL, { useUnifiedTopology: true })
   .then(client => {
     moviedb = client.db('moviedb')
-    console.log('Connected to MongoDB successfully!');
+    console.log('Connected to MongoDB successfully!')
     users = moviedb.collection('usercollection')
     admins = moviedb.collection('admincollection')
     theatresObj = moviedb.collection('theatrecollection')
