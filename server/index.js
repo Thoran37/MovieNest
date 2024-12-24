adc7e8aedbdee0fd47ede37ae0c2fe1d928e8c26
Thoran37 <thoranmuvvala@gmail.com>
2024-12-24 15:40:35 +0530

    added links in cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(morgan("common"));
 // CORS
 const cors = require('cors')
 app.use(cors({
-  origin: ["https://movienest-nine.vercel.app/"],
+  origin: ["https://movienest-nine.vercel.app/", "https://movie-nest-three.vercel.app/"],
   methods: ["POST", "GET"],
   credentials: true
 }));
