b3bfdbd91ec55c3f9c656a7bc0d2a68b35d4c8f5
Thoran37 <thoranmuvvala@gmail.com>
2024-12-24 11:46:24 +0530

    Added client link in sever

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(morgan("common"));
 // CORS
 const cors = require('cors')
 app.use(cors({
-  origin: ["movienest-nine.vercel.app"],
+  origin: ["https://movienest-nine.vercel.app/"],
   methods: ["POST", "GET"],
   credentials: true
 }));
