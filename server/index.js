9eb1fda3294a7d6630768372a5a46bb8f236a3bc
Muvvala Thoran Chandra <132071612+Thoran37@users.noreply.github.com>
2024-12-24 10:21:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use('/assets', exp.static(path.join(__dirname, 'public/assets')));
 
 // Database connection
 let moviedb, users, admins, theatresObj, moviesObj, shows, reservations
-mongodb.connect('mongodb+srv://thoran:qwertasd@moviebookingcluster.ploz5ax.mongodb.net/')
+mongodb.connect(process.env.DB_URL)
   .then(client => {
     moviedb = client.db('moviedb')
     users = moviedb.collection('usercollection')
